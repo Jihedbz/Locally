59acf2ca4a330a88420c9673253f2b7cbac69d57
Jihed Bouazizi <Jih3db@gmail.com>
2025-03-28 23:10:33 +0100

    testing

--- src/pages/Projects/main.tsx
+++ src/pages/Projects/main.tsx
@@ -112,7 +112,7 @@ const Projects = () => {
     {
       id: "actions",
       enableHiding: false,
-      cell: ({ row }) => {
+      cell: ({ }) => {
         // const project = row.original; // Removed unused variable
         return (
           <Button variant="ghost" className="h-8 w-8 p-0">
